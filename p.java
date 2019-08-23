262764944728fa6f4a08e08de477026e2fa42d6a
anvithapo99 <53563539+anvithapo99@users.noreply.github.com>
2019-08-23 16:20:46 +0530

    Update p.java

--- p.java
+++ p.java
@@ -1,3 +1,11 @@
+ Micro and Array Update
+Micro purchased an array A having N integer values. After playing it for a while, he got
+bored of it and decided to update value of its element. In one second he can increase
+value of each array element by 1. He wants each array element's value to become greater
+than or equal to K. Please help Micro to find out the minimum amount of time it will take,
+for him to do so.
+
+
 import java.util.*;
 
  
@@ -71,3 +79,12 @@ class TestClass {
     }
 
 }
+sample input             
+2
+3 4
+1 2 5
+3 2
+2 5 5
+sample ouput
+3
+0
